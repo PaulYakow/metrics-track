ab010a0fe56cd8d6191cfa718dc7cbab119910d8
PaulYakow <paul.yakow@gmail.com>
2022-11-30 20:14:31 +0500

    + cmd/agent, cmd/server - добавлены глобальные переменные с информацией о версии, дате и коммите (а также вывод в stdout сообщения с этой информацией)

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"strings"
 
 	"github.com/PaulYakow/metrics-track/config"
 	"github.com/PaulYakow/metrics-track/internal/app/client"
 )
+
+/*
+  Build version: <buildVersion> (или "N/A" при отсутствии значения)
+  Build date: <buildDate> (или "N/A" при отсутствии значения)
+  Build commit: <buildCommit> (или "N/A" при отсутствии значения)
+*/
+
+var buildVersion string
+var buildDate string
+var buildCommit string
 
 func main() {
 	cfg, err := config.NewClientConfig()
@@ -14,5 +26,42 @@ func main() {
 		return
 	}
 
+	bi := buildInfo{}
+	bi.printInfo()
+
 	client.Run(cfg)
+}
+
+type buildInfo struct {
+	buildVersion string
+	buildDate    string
+	buildCommit  string
+}
+
+func (bi *buildInfo) printInfo() {
+	var sb strings.Builder
+
+	bi.buildVersion = "N/A"
+	bi.buildDate = "N/A"
+	bi.buildCommit = "N/A"
+
+	switch {
+	case buildVersion != "":
+		bi.buildVersion = buildVersion
+	case buildDate != "":
+		bi.buildDate = buildDate
+	case buildCommit != "":
+		bi.buildCommit = buildCommit
+	}
+
+	sb.WriteString("Build version: ")
+	sb.WriteString(bi.buildVersion)
+	sb.WriteString("\n")
+	sb.WriteString("Build date: ")
+	sb.WriteString(bi.buildDate)
+	sb.WriteString("\n")
+	sb.WriteString("Build commit: ")
+	sb.WriteString(bi.buildCommit)
+	sb.WriteString("\n")
+	fmt.Println(sb.String())
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"strings"
 
 	"github.com/PaulYakow/metrics-track/config"
 	"github.com/PaulYakow/metrics-track/internal/app/server"
 )
+
+/*
+  Build version: <buildVersion> (или "N/A" при отсутствии значения)
+  Build date: <buildDate> (или "N/A" при отсутствии значения)
+  Build commit: <buildCommit> (или "N/A" при отсутствии значения)
+*/
+
+var buildVersion string
+var buildDate string
+var buildCommit string
 
 func main() {
 	cfg, err := config.NewServerConfig()
@@ -13,5 +25,42 @@ func main() {
 		log.Fatalf("server - create config: %v\n", err)
 	}
 
+	bi := buildInfo{}
+	bi.printInfo()
+
 	server.Run(cfg)
+}
+
+type buildInfo struct {
+	buildVersion string
+	buildDate    string
+	buildCommit  string
+}
+
+func (bi *buildInfo) printInfo() {
+	var sb strings.Builder
+
+	bi.buildVersion = "N/A"
+	bi.buildDate = "N/A"
+	bi.buildCommit = "N/A"
+
+	switch {
+	case buildVersion != "":
+		bi.buildVersion = buildVersion
+	case buildDate != "":
+		bi.buildDate = buildDate
+	case buildCommit != "":
+		bi.buildCommit = buildCommit
+	}
+
+	sb.WriteString("Build version: ")
+	sb.WriteString(bi.buildVersion)
+	sb.WriteString("\n")
+	sb.WriteString("Build date: ")
+	sb.WriteString(bi.buildDate)
+	sb.WriteString("\n")
+	sb.WriteString("Build commit: ")
+	sb.WriteString(bi.buildCommit)
+	sb.WriteString("\n")
+	fmt.Println(sb.String())
 }
